581856d28af9d48e6e38c982d6620403b9da7aeb
EricRAbib <eric.ra1@puccampinas.edu.br>
2023-11-28 13:23:24 -0300

    Tentando arrumar a conexao com o localhost

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,26 +1,27 @@
-import axios from 'axios';
-
 // Define a URL que você deseja acessar
-const url = 'http://localhost:8080/login';
+const url = "http://localhost:8080/login"
 
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+function EnviarLogin()
+  {
+      const email = document.getElementById('email');
+      const password = document.getElementById('password');
+      
+      
+      const data = JSON.stringify({
+        "email": email,
+        "password": password
+      });
 
-const data = {
-  email: email,
-  password: password
-};
-
-// Faz uma requisição GET para a URL
-axios.post(url, data)
-  .then(response => {
+      axios.post(url, data)
+      .then(response => {
         console.log('Sucesso', response.data);
         if(response.data === 200){
           window.location.href = 'main_page.html';
         }else{
-          console.error('Erro no login', response.data);
+          console.log('Erro no login', response.data);
         }
       })
       .catch(error =>{
-        console.error('Erro:', error);
+        console.log(error);
       });
+    }
